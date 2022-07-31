14553f0ade1f7abc74cbad27e9148ba09a627357
Luigi Carvalho <carvalholuigi25@gmail.com>
2022-07-31 14:17:20 +0100

    improvements for login in client and server of website

--- Website/Client/Program.cs
+++ Website/Client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { 
+builder.Services.AddScoped(sp => new HttpClient {
     //BaseAddress = new Uri("https://localhost:5001/")
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
 });
